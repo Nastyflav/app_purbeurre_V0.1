95d82701789dae37c54a7f0d08d9aa09344756ec
Flavien <flavien.murail@gmail.com>
2020-04-19 16:34:50 +0200

    Downloaded all the needed scripts for the html base

--- app_purbeurre/app_purbeurre/urls.py
+++ app_purbeurre/app_purbeurre/urls.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="base.html"), name="base"),
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
     path('mentions-legales/', TemplateView.as_view(template_name="terms.html"), name="terms"),
     # path('authentication/', include("authentication.urls")),
     # path('save/', include("save.urls")),
