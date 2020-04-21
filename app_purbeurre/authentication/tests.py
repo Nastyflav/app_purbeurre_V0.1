f3ca5be69438a06ca7b7245d1a635c88d88793cf
Flavien <flavien.murail@gmail.com>
2020-04-21 16:19:56 +0200

    The authentication tests are working fine, except for the login redirection

--- app_purbeurre/authentication/tests.py
+++ app_purbeurre/authentication/tests.py
@@ -9,6 +9,7 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.db import IntegrityError
 from .models import User
 
 
@@ -21,11 +22,82 @@ class TestAuthentication(TestCase):
         self.logout_url = reverse('authentication:logout')
         self.signup_url = reverse('authentication:signup')
         self.profile_url = reverse('authentication:profile')
-        self.user = User.objects.create(email='user@test.dj', firstname='John', lastname='Doe')
+        self.user = User.objects.create(email='user@test.dj')
         self.user.set_password('supertest2020')
         self.user.save()
 
-    def test_login_url_and_template(self):
+    def test_login_page_returns_200(self):
+        """To test the status code and the login page"""
         response = self.client.get(self.login_url)
         self.assertTemplateUsed(response, 'registration/login.html')
         self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_returns_200(self):
+        """To test the status code and the signup page"""
+        response = self.client.get(self.signup_url)
+        self.assertTemplateUsed(response, 'registration/account_creation.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_page_returns_200(self):
+        """To test the status code and the profile page"""
+        response = self.client.get(self.profile_url)
+        self.assertTemplateUsed(response, 'registration/user_profile.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_is_a_valid_action(self):
+        """To test redirection when login action is completed"""
+        response = self.client.post(self.login_url, {
+            'username': 'user@test.dj',
+            'password': 'supertest2020'
+        })
+        self.assertRedirects(
+            response, '/', status_code=302, target_status_code=200)
+
+    def test_logout(self):
+        """To test the redirection when the user logs out"""
+        response = self.client.get(self.logout_url)
+        self.assertRedirects(
+            response, '/', status_code=302, target_status_code=200)
+
+    def test_signup_when_user_creation_is_ok(self):
+        """Test when the creation of new user is authorized""" 
+        new_user = 'newuser@test.dj'
+        user = User.objects.create(email=new_user)
+        user.set_password('@supernouveau')
+        user.save()
+
+        user = User.objects.get(email=new_user)
+
+        self.assertIsInstance(user, User)
+        self.assertEqual(str(user), new_user)
+
+    def test_signup_is_a_valid_action(self):
+        """To check the redirection when the account creation is accepted"""
+        response = self.client.post(self.signup_url, {
+            'email': 'newuser@test.dj',
+            'password1': 'supernouveau',
+            'password2': 'supernouveau'
+        })
+        self.assertRedirects(
+            response, '/', status_code=302, target_status_code=200)
+
+    def test_signup_form_is_not_valid(self):
+        """To check if the user can retry to sign up if he fails once"""
+        response = self.client.post(self.signup_url, {
+            'email': 'newuser',
+            'password1': 'supernouveau',
+            'password2': 'supernouveau1'
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_when_user_already_exists(self):
+        """Check if a similar user or a similar password already exists"""
+        try:
+            user = User.objects.create(email='user@test.dj')
+            user.set_password('supernouveau')
+            user.save()
+
+        except IntegrityError:
+            user = False
+
+        self.assertFalse(user)
--- app_purbeurre/authentication/views.py
+++ app_purbeurre/authentication/views.py
@@ -10,23 +10,42 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import FormView
-from .forms import SignUpForm
+from .forms import SignUpForm, LogInForm
 
 
 class SignUp(FormView):
-    """Show form and create user if valid."""
+    """Show the form and create a user if its a valid one"""
 
+    template_name = 'registration/account_creation.html'
     form_class = SignUpForm
     success_url = '/'
-    template_name = 'registration/account_creation.html'
 
     def form_valid(self, form):
-        """ if form is valid """
+        """if the form is validated"""
 
         form.save()
         email = form.cleaned_data.get('email')
         password = form.cleaned_data.get('password1')
         user = authenticate(email=email, password=password)
+        if user:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+class LogIn(FormView):
+    """Show the form and log a user if the form is valid"""
+
+    template_name = 'registration/login.html'
+    form_class = LogInForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        """if the form is validated"""
+
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
         if user:
             login(self.request, user)
         return super().form_valid(form)
