004c24d6fa53ed127bf1116af87bead1479fb650
Flavien <flavien.murail@gmail.com>
2020-04-23 16:23:12 +0200

    I keep on my first thought : 3 apps

--- app_purbeurre/search/management/commands/db_init.py
+++ app_purbeurre/search/management/commands/db_init.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-Author: [Nastyflav](https://github.com/Nastyflav) 2020-04-20
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-04-23
 Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 """
--- app_purbeurre/search/models.py
+++ app_purbeurre/search/models.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-04-20
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+"""
+
 from django.db import models
 
-# Create your models here.
+
