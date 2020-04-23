13a46ce54b1f58e2e852c90f335fc0c69c754a6f
Flavien <flavien.murail@gmail.com>
2020-04-23 11:07:26 +0200

    The login function is working

--- app_purbeurre/app_purbeurre/settings.py
+++ app_purbeurre/app_purbeurre/settings.py
@@ -129,6 +129,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static" ),)
 
-LOGIN_REDIRECT_URL = "/profil/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/authentication"
