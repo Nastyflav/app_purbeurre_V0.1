15e4ff0ab9ad8f60401206b0702ca18691d9091b
Flavien <flavien.murail@gmail.com>
2020-04-23 16:03:38 +0200

    Created the commands folder to initialize the DB

---
+++ app_purbeurre/search/management/commands/db_init.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-04-20
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db import IntegrityError
