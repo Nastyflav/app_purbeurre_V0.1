bc68fc37039fe4b3e47b9c69b2fa77eb77cdd2bf
Flavien <flavien.murail@gmail.com>
2020-04-24 11:48:26 +0200

    Started to work on the db command

--- app_purbeurre/search/management/commands/db_init.py
+++ app_purbeurre/search/management/commands/db_init.py
@@ -9,3 +9,36 @@ Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
+import requests
+from ...models import Category, Product
+from .constants import API_CATEGORIES, API_PAGE_SIZE, API_PAGES_NUMBER, API_URL_SOURCE
+
+
+class Command(BaseCommand):
+    """Command class for custom django commands"""
+    help = "Load datas from the OFF API to our database"
+
+    def add_arguments(self, parser):
+        """Set the enabled arguments for the command"""
+        parser.add_argument("-c", "--category", type=str)
+        parser.add_argument("-p", "--product", type=str)
+
+    def handle(self, *args, **options):
+        """Requests the API then fills the DB"""
+        if not isinstance(options["category"], str):
+            raise TypeError("Entrez une chaine de caractère.")
+
+        if not isinstance(options["product"], str):
+            raise TypeError("Entrez une chaine de caractère.")
+
+        if options["category"] and options["product"]:
+            self.category = options["category"]
+            self.product = options["product"]
+            self.data_loading()
+
+    def data_loading(self):
+        """Request to the OFF API"""
+        self.categories = API_CATEGORIES
+        self.stdout.write("Loading data...")
+
+
--- app_purbeurre/search/tests.py
+++ app_purbeurre/search/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+#! /usr/bin/env python3
+# coding: utf-8
 
-# Create your tests here.
+"""
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-04-24
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+"""
+
+from django.test import TestCase, Client, TransactionTestCase
+from django.db import IntegrityError
+from django.urls import reverse
+from .models import Category, Product
+from authentication.models import User
+from django.core.management import call_command
+from unittest.mock import patch
