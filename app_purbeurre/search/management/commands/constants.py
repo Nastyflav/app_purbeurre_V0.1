90bd4178d4c385d207928dbdd830ef0168b5efa1
Flavien <flavien.murail@gmail.com>
2020-04-24 15:13:27 +0200

    Everything is up to date

---
+++ app_purbeurre/search/management/commands/constants.py
@@ -0,0 +1,8 @@
+#API search parameters
+
+API_PAGE_SIZE = 150
+API_PAGES_NUMBER = 1
+API_CATEGORIES = ['Pâte à tartiner', 'Thés en sachet', 'Fromages blancs', 'Jus de fruits', 'Confitures de fruits',
+                 'Céréales pour petit-déjeuner', 'Cafés', 'Pâtes alimentaires', 'Epices', 'Légumes surgelés',
+                 'Sauces tomates', 'Surimi', 'Légumineuses', 'Frites', 'Galettes de céréales soufflées']
+API_URL_SOURCE = 'https://fr.openfoodfacts.org/cgi/search.pl?'
