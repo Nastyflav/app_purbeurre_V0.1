c519b7701e6f5036d5ef6c675120e704ba757269
Flavien <flavien.murail@gmail.com>
2020-05-03 16:33:07 +0200

    PEP8 is OK for the main app

--- app_purbeurre/app_purbeurre/settings.py
+++ app_purbeurre/app_purbeurre/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'app_purbeurre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static" ),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
--- app_purbeurre/app_purbeurre/urls.py
+++ app_purbeurre/app_purbeurre/urls.py
@@ -20,7 +20,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"), name="index"),
-    path('mentions-legales/', TemplateView.as_view(template_name="terms.html"), name="terms"),
+    path('mentions-legales/', TemplateView.as_view(template_name="terms.html"),
+         name="terms"),
     path('authentication/', include("authentication.urls")),
     path('search/', include("search.urls")),
     path('save/', include("save.urls")),
